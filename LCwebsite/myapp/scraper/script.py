3b14ca5b787ac45d0886d4790dca0cece62aa933
Timberland <ryskeldiev.tema@gmail.com>
2026-06-18 16:59:37 -0500

    refactor: add more concurrent scraper tabs
    -Going back to 5 concurrent tabs

--- LCwebsite/myapp/scraper/script.py
+++ LCwebsite/myapp/scraper/script.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 from django.utils import timezone
 import gc
 
-NUM_STORES_TO_SCRAPE = 3
+NUM_STORES_TO_SCRAPE = 5
 
 logger = logging.getLogger(__name__)
 def logging_setup():

bdb2ddcd667bdd398e6d4b608a1f8144d7730097
Timberland <ryskeldiev.tema@gmail.com>
2026-06-18 11:58:53 -0500

    refactor settings.py

--- LCwebsite/LCwebsite/settings.py
+++ LCwebsite/LCwebsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['api.pizzascanner.org', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['api.pizzascanner.org', 'pizzascanner.org', 'localhost', '127.0.0.1']
 
 
 # Application definition

012f291a4c66d0c0fc458fb71e119cc3ebea877c
Timberland <ryskeldiev.tema@gmail.com>
2026-06-18 17:08:45 -0500

    fix: allow server to host
    -Added new IP to allowed hosts and hid it from settings.py

--- LCwebsite/LCwebsite/settings.py
+++ LCwebsite/LCwebsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'False'
 
-ALLOWED_HOSTS = ['api.pizzascanner.org', 'pizzascanner.org', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition

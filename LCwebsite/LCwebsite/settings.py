501d104bd074f904b64bd8183d784dd2267995e3
Timberland <ryskeldiev.tema@gmail.com>
2026-06-18 01:06:50 -0500

    Add website domains

--- LCwebsite/LCwebsite/settings.py
+++ LCwebsite/LCwebsite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['api.pizzascanner.org', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -130,9 +130,13 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://pizzascanner.org",
+    "https://www.pizzascanner.org",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://pizzascanner.org",
+    "https://www.pizzascanner.org",
 ]

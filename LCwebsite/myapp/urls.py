cdd7975cd72d7177d220f53b87793db2d5137580
Timberland <ryskeldiev.tema@gmail.com>
2026-06-18 20:58:41 -0500

    fix: resolve 404 error on database page

--- LCwebsite/myapp/urls.py
+++ LCwebsite/myapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.scraper_api, name='index'),
     path('stores/', views.dashboard_api, name='api_view'),
     path('api/check_scrape_status/', views.check_scrape_status, name='check_scrape_status'),
     path('api/db_version/', views.get_db_version, name='db_version'),
+    path('', views.scraper_api, name='index'),
 ]

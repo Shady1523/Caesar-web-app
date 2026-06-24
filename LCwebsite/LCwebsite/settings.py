1ed8d74ebf21215e90c26c029be5a5e3d681c911
Timberland <ryskeldiev.tema@gmail.com>
2026-06-24 00:19:35 -0500

    fix: update redis URL

--- LCwebsite/LCwebsite/settings.py
+++ LCwebsite/LCwebsite/settings.py
@@ -148,7 +148,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379/0")],
+            "hosts": [os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")],
         },
     },
 }
